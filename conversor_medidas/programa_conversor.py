623188560d0751220cea72057ce4aa4481d9fab1
edengole31 <egonzalez.tecnico@gmail.com>
2025-04-12 02:53:13 -0300

    probando hacer modificaciones

--- conversor_medidas/programa_conversor.py
+++ conversor_medidas/programa_conversor.py
@@ -6,6 +6,5 @@ def cm_a_pulgadas(cm):
 df = pd.read_excel("herramientas_medidas.xlsx")
 
 #Anadir una columna en pulgadas donde se colocara la conversion
-
 df["pulgadas"]=df["Centimetros"].apply(cm_a_pulgadas)
 df.to_excel("herramientas_medidas_pulgadas.xlsx",index=False)
